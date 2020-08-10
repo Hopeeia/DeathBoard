380a7efa4fb377fb66cbbbfc73db6622b219df97
ShrBox <shrbox@outlook.com>
2020-08-10 15:58:07 +0800

    init

---
+++ DeathBoard/DeathBoard.cpp
@@ -0,0 +1,58 @@
+#include "pch.h"
+
+bool DeathBoard_sidebar;
+
+void loadConfig()
+{
+	std::string config_file = "config\\deathboard.json";
+	std::ifstream fs;
+	fs.open(config_file, std::ios::in);
+	DeathBoard_sidebar = false;
+	if (!fs)
+	{
+		std::cout << "[DeathBoard] " << config_file << " not found, creating file(default value used)\n";
+		std::ofstream of(config_file);
+		if (of)
+		{
+			of << "{\n\"enableSidebar\": true\n}";
+		}
+		else
+		{
+			std::cout << "[DeathBoard] " << "config file creation failed, plase create \"config\" folder\n";
+		}
+	}
+	else
+	{
+		std::string json;
+		char buf[1024];
+		while (fs.getline(buf, 1024))
+		{
+			json.append(buf);
+		}
+		rapidjson::Document document;
+		document.Parse(json.c_str());
+		DeathBoard_sidebar = document["enableSidebar"].GetBool();
+	}
+}
+
+void entry()
+{
+	loadConfig();
+	addListener([](PlayerDeathEvent& ev)
+	{
+		BDX::runcmdEx("scoreboard players add \"" + ev.getPlayer().getRealName() + " \" deathboard 1");
+	});
+	addListener([](ServerStartedEvent& ev)
+	{
+		BDX::runcmd(u8"scoreboard objectives add deathboard dummy ËÀÍö°ñ");
+		if (DeathBoard_sidebar) BDX::runcmd("scoreboard objectives setdisplay sidebar deathboard ascending");
+	});
+	std::cout << "[DeathBoard] Loaded!\n";
+}
+
+/*
+THook(void, "?onPlayerDeath@Level@@UEAAXAEAVPlayer@@AEBVActorDamageSource@@@Z", void* lev, void* player, void* damage)
+{
+	std::cout << "death\n";
+	return original(lev, player, damage);
+}*/
---
+++ README.md
@@ -0,0 +1,26 @@
+# DeathBoard
+
+极为简单的死亡榜插件  
+目前只支持BedrockX 0711及以上
+
+## Download
+
+https://wwa.lanzous.com/b07rtz2hi  
+密码:gbg7
+
+## Configuration file
+
+`config/deathboard.json`:
+```
+{
+"enableSidebar": true //是否在启动时自动将死亡绑设置到sidebar
+}
+```
+
+## License
+
+MIT
+
+## Credits
+
+rapidjson, BedrockX
